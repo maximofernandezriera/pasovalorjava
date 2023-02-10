62faf54b3bb1dc6e5f68a7a08a322240967de108
Máximo Fernández Riera <43608040+maximofernandezriera@users.noreply.github.com>
2023-02-10 18:00:42 +0100

    Update ValueOrReference2.java

--- ValueOrReference2.java
+++ ValueOrReference2.java
@@ -7,7 +7,15 @@ public class ValueOrReference{
          String cadena[]= new String[10];
          cadena[0]="hola";
          pasarPorValor(cadena);
-         System.out.println("Fuera la cadena vale: " + cadena[0]);
+          System.out.println("Fuera la cadena vale: " + cadena[0]);
+         
+          System.out.println("PasarValorObjeto");
+          pasarPorValorObjeto(cadena);
+            System.out.println("fuera la cadena vale: " + cadena[0]);
+             System.out.println(" ");
+              System.out.println("la referencia de cadena vuele a ser " + cadena);
+         
+         
      }
      
      public static void pasarPorValor(int numero){
@@ -18,5 +26,16 @@ public class ValueOrReference{
      public static void pasarPorValor(String[] cadena){
          cadena[0]="adios";
           System.out.println("Dentro del metodo cadena de 0 vale: " + cadena[0]);
+     }
+     
+     public static void pasarPorValorObjeto(String[] cad){
+         String cadena2[] = new String[10];
+         cadena2[0]="adios";
+          System.out.println("La referencia de memoria  de cadena es: " + cad);
+           System.out.println("La referencia de memoria de cadena2 es: " + cadena2);
+           cad=cadena2;
+           System.out.println("La nueva referencia de memoria de cadena2 es: " + cadena2);
+           System.out.println("Dentro del metodo vale: " + cadena2);
+           
      }
 }
